0ca06ad2e41f62dc3b4279e8c6ee2f2425731bbe
ritabc <ritabc@gmail.com>
2018-08-11 21:59:44 -0700

    add search by doctor name functionality, empty output after each search

--- src/better-doctor.js
+++ src/better-doctor.js
@@ -32,4 +32,21 @@ export class BetterDoctorAPI {
       request.send();
     })
   }
+
+  getDoctorsByName(name) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&user_key=${process.env.API_KEY}`;
+
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    })
+  }
 }
--- src/main.js
+++ src/main.js
@@ -22,15 +22,15 @@ $(document).ready(function() {
 
     $('#ailments-search').click(function(e) {
       e.preventDefault();
+      $('.card-columns').empty();
       let ailment = $('#ailments-dropdown option:selected').val();
       let doctorsFromAilment = newBetterDoctor.getDoctorsByAilment(ailment);
 
       doctorsFromAilment.then(function(response) {
         let doctorsObjectsFromAilmentsBody = JSON.parse(response);
         let doctorObjectsArray = doctorsObjectsFromAilmentsBody.data
-        console.log(doctorObjectsArray)
         doctorObjectsArray.forEach(function(doctor) {
-          let name = doctor.profile.first_name + doctor.profile.last_name + ", " + doctor.profile.title
+          let name = doctor.profile.first_name + " " + doctor.profile.last_name + ", " + doctor.profile.title
           let mainPractice = doctor.practices[0]
           let address = ""
           if (doctor.practices[0].visit_address.hasOwnProperty('street2')) {
@@ -79,5 +79,65 @@ $(document).ready(function() {
         })
       });
     });
+
+    $('#name-search').click(function(e) {
+      e.preventDefault();
+      $('.card-columns').empty();
+      let nameToSearch = $('#name').val();
+      $('#name').val('');
+      let doctorsFromName = newBetterDoctor.getDoctorsByName(nameToSearch);
+      doctorsFromName.then(function(response) {
+        let doctorsObjectsFromNameSearchBody = JSON.parse(response);
+        let doctorObjectsArray = doctorsObjectsFromNameSearchBody.data
+        doctorObjectsArray.forEach(function(doctor) {
+          let name = doctor.profile.first_name + " " + doctor.profile.last_name + ", " + doctor.profile.title
+          let mainPractice = doctor.practices[0]
+          let address = ""
+          if (doctor.practices[0].visit_address.hasOwnProperty('street2')) {
+            address = doctor.practices[0].visit_address.street + " " + mainPractice.visit_address.street2 + ", " + mainPractice.visit_address.city + ", " + mainPractice.visit_address.state + " " + mainPractice.visit_address.zip
+          } else {
+            address = mainPractice.visit_address.street + ", " +  mainPractice.visit_address.city + ", " + mainPractice.visit_address.state + " " + mainPractice.visit_address.zip
+          }
+          let phones = mainPractice.phones
+          let landlines = phones.filter(function(phone) {
+            if (phone.type == "landline") {
+              return true;
+            } else {
+              return false;
+            }
+          });
+          let phoneOrPhones = ""
+          landlines.forEach(function(landline) {
+            phoneOrPhones = phoneOrPhones + landline.number + "\n"
+          })
+          let website = ""
+          if (mainPractice.hasOwnProperty("website")) {
+            website = mainPractice.website
+          }
+          else {
+            website = "None Listed"
+          }
+          let newPatientsString = ""
+          if (mainPractice.accepts_new_patients) {
+            newPatientsString = 'Yes'
+          } else {
+            newPatientsString = 'No'
+          }
+          $('.doctors-by-ailment > .card-columns').append(`
+            <div class="card card-body">
+              <h3 class="card-title">Main Practice of ${name}</h3>
+              <h4>Address</h4>
+              <p>${address}</p>
+              <h4>Phone(s)</h4>
+              <p>${phoneOrPhones}</p>
+              <h4>Website</h4>
+              <p>${website}</p>
+              <h4>Accepting New Patients?</h4>
+              <p>${newPatientsString}</p>
+            </div>
+            `)
+        })
+      })
+    })
   });
 });
